6428a93b9e82ca651cc9d9b7913eb49b4b7a57d5
lon <lon@cscap.myguest.virtualbox.org>
2023-06-03 17:11:27 +0800

    meow finish

--- meow/solve.sh
+++ meow/solve.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-info="$(xxd -p meow.jpg | tail -n 8 | tr -d '\n' | cut -c 53-)"
-echo "$info" | xxd -r -p > compressed.bin
-unzip compressed.bin > /dev/null
-cat flag.txt | grep -o 'FLAG{[^}]*}'
-rm *.txt *.bin
+
+binwalk -e $1 > /dev/null
+cat _meow.jpg.extracted/flag.txt | grep -o 'FLAG{.*}'
+rm -r _meow.jpg.extracted
