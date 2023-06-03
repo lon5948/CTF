09fb3355feafbdbf9438251a8a4475a5e8acbbe9
lon <lon@cscap.myguest.virtualbox.org>
2023-06-03 15:31:49 +0800

    capture the flag of random

--- random/solve.py
+++ random/solve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import pwn, sys, datetime, subprocess
+import pwn, sys, datetime, time, subprocess
 
 if __name__ == '__main__':
     target = None
@@ -14,12 +14,12 @@ if __name__ == '__main__':
     recv = target.recvuntil(b'password:').decode("utf-8", "ignore").strip()
     recv_time = recv.split(' ')[0].split(':')
     hour, minute, second = int(recv_time[0]), int(recv_time[1]), int(recv_time[2])
-    
+
     today = datetime.date.today()
     year, month, day = today.year, today.month, today.day
     date = datetime.datetime(year, month, day, hour, minute, second)
-    timestamp = int(date.timestamp)
+    timestamp = int(date.timestamp())
 
-    attack = subprocess.run(['./sol.c', str(timestamp)], stdout=subprocess.PIPE).stdout.strip()
+    attack = subprocess.run(['./sol', str(timestamp)], stdout=subprocess.PIPE).stdout.strip()
     target.sendline(attack)
     print(target.recvall().decode("utf-8", "ignore").strip())
